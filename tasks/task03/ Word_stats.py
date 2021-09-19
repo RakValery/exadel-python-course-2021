691388d08dd0f2d133178d0165b9db43338265fa
Valery Rak <vrak@exadel.com>
2021-09-19 16:46:22 +0300

    Task 03: Word stats v2.1

--- tasks/task03/ Word_stats.py
+++ tasks/task03/ Word_stats.py
@@ -1,27 +1,16 @@
 import re
 texts = ["Hello, World!", "The world is mine", "Hello, how are you?"]
-liststr = []
+dictw = {}
+i = 0
 for istr in texts:
-    istr = re.split(r'[:; ,*.!?\n]+',istr.lower())
-    liststr.append(istr)
-print("word\t\tcount\tfirst line\n----------------------------------")
-i = 0 
-for istr in liststr:
-    j = 0
-    for iword in istr:
+    for iword in re.split(r'[:; ,*.!?\n]+',istr.lower()):
         if iword == "":
-            j += 1
             continue
-        count = 1
-        for a in range(i, len(liststr)):
-            if (a == i):
-                startb = j+1
-            else:
-                startb = 0
-            for b in range(startb, len(liststr[a])):
-                if liststr[a][b] == iword:
-                    count += 1
-                    liststr[a][b] = ""
-        j += 1
-        print(f"{iword}\t\t{count}\t{i}")
+        if dictw.get(iword, "e404") == "e404":
+            dictw[iword] = [1, i]
+        else:
+            dictw[iword][0] += 1
     i += 1
+print("word\t\tcount\tfirst line\n----------------------------------")
+for iword in dictw.keys():
+    print(f"{iword}\t\t{dictw.get(iword)[0]}\t{dictw.get(iword)[1]}")
