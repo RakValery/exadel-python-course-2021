3639e0498e35c4649da0b65c9397f2b354e62493
Valery Rak <vrak@exadel.com>
2021-09-20 10:56:21 +0300

    Task 03: Word stats v2.2

--- tasks/task03/ Word_stats.py
+++ tasks/task03/ Word_stats.py
@@ -1,16 +1,14 @@
 import re
 texts = ["Hello, World!", "The world is mine", "Hello, how are you?"]
 dictw = {}
-i = 0
-for istr in texts:
+for i, istr in enumerate(texts):
     for iword in re.split(r'[:; ,*.!?\n]+',istr.lower()):
         if iword == "":
             continue
-        if dictw.get(iword, "e404") == "e404":
+        if iword not in dictw:
             dictw[iword] = [1, i]
         else:
             dictw[iword][0] += 1
-    i += 1
 print("word\t\tcount\tfirst line\n----------------------------------")
-for iword in dictw.keys():
-    print(f"{iword}\t\t{dictw.get(iword)[0]}\t{dictw.get(iword)[1]}")
+for iword, (count, line) in dictw.items():
+    print(f"{iword}\t\t{count}\t{line}")
