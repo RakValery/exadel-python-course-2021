acc8419a2221d5c80bd78b0171d0cba497409db9
Valery Rak <vrak@exadel.com>
2021-09-27 14:51:39 +0300

    Task 05: User factory v1.5

--- tasks/task05/user_factory.py
+++ tasks/task05/user_factory.py
@@ -1,22 +1,13 @@
 #Task 05: User factory
-def create_user(*arg, **kwarg):
-    if len(arg) == 2 or (len(arg) == 3 and type(arg[2]) != int):
-        name = arg[0]
-        surname = arg[1]
-        age = 42
-    elif len(arg) == 3 and type(arg[2]) == int:
-        name = arg[0]
-        surname = arg[1]
-        age = arg[2]
-    else: 
-        raise ValueError(f"ValueError: arg={arg}")
+def create_user(name, surname, age = 42, **kwarg):
+    if type(name) != str or type(surname) != str or type(age) != int:
+        raise ValueError("ValueError: Type of name and surname must be string, type of age must be int")
     return {
        "name": name,
        "surname": surname,
        "age": age,
        "extra": kwarg
        }
-
 tests = [
     [["John", "Doe"], {}, #1
     {
@@ -44,30 +35,26 @@ tests = [
            "won_nobel": True}
         }
     ],
-    [["Vasya", "Pupkin", "2"], {"education": "absent", "won_nobel": "bad luck"}, #4 Wrong age = 42
-    {
-       "name": "Vasya",
-       "surname": "Pupkin",
-       "age": 42,
-       "extra": {
-           "education": "absent",
-           "won_nobel": "bad luck"}
-        }
+    [["Vasya", "Pupkin", "two"], {"education": "absent", "won_nobel": "bad luck"}, #4 Wrong age = 42
+    "ValueError: Type of name and surname must be string, type of age must be int"
     ],
     [[2], {}, #5 Name and surname are missed
-    "ValueError: arg=(2,)"
+    "create_user() missing 1 required positional argument: 'surname'"
     ]
 ]
 for i, test in enumerate(tests):
     result = False
     print(f"Test {(i+1)} ...")
     try:
-        res = create_user(*test[0], **test[1])
-        if res == test[2]:
+        if create_user(*test[0], **test[1]) == test[2]:
             result = True
         print("OK")
+    except TypeError as err:
+        if str(err) == test[2]:
+            result = True
+        print(f"{err} - OK")
     except ValueError as err:
         if str(err) == test[2]:
             result = True
-            print("OK")
+        print(f"{err} - OK")
     assert result
