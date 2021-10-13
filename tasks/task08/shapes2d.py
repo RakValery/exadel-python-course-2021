b57810ad4c5762680893a611bc5c9483f21d46bd
Valery Rak <vrak@exadel.com>
2021-10-13 13:29:15 +0300

    Task 08: Class - inheritance, magic methods v1.0 with tests v1.0

--- tasks/task08/shapes2d.py
+++ tasks/task08/shapes2d.py
@@ -2,7 +2,7 @@
 #Implement a set of classes for 2D shapes manipulations
 from typing import NamedTuple
 import abc
- 
+import math 
  
 class Point2D(NamedTuple):
     """
@@ -39,19 +39,85 @@ class Shape2D(abc.ABC):
         raise NotImplementedError
 
 class Rectangle(Shape2D): #Rectangle - inherits from Shape2D. Has arguments like bottom_left, width, length.
-    pass
+    def __init__(self, bottom_left: Point2D, width: float, length: float):
+        super().__init__()
+        if type(bottom_left) is Point2D and isinstance(width, (int, float)) and isinstance(length, (int, float)) and  \
+            width > 0 and length > 0:###
+            self.bottom_left = bottom_left
+            self.width = width
+            self.length = length
+        else:
+            raise ValueError
+    
+    def __str__(self):
+        return f"Rectangle: bottom_left = {self.bottom_left}, width = {self.width}, length = {self.length}"
 
-class Square(Shape2D): #Square - inherits from Rectangle.
-    pass
+    def __contains__(self, point: Point2D):
+        if type(point) is Point2D and (self.bottom_left.x <= point.x <= self.bottom_left.x + self.length) and (self.bottom_left.y <= point.y <= self.bottom_left.y + self.width):
+            return True
+        else: 
+            return False
+    
+    def area(self):
+        return self.width * self.length
+
+class Square(Rectangle): #Square - inherits from Rectangle.
+    def __init__(self, bottom_left: Point2D, side: float):
+        if type(bottom_left) is Point2D and isinstance(side, (int, float)) and  side > 0:
+            super().__init__(bottom_left, side, side)
+            self.side = side
+        else:
+            raise ValueError
+    
+    def __str__(self):
+        return f"Square: bottom_left = {self.bottom_left}, side = {self.side}"
+    
 class Circle(Shape2D): #Circle - inherits from Shape2D. Has arguments like center, radius.
-    pass
+    def __init__(self, center: Point2D, radius: float):# -> None:
+        super().__init__()
+        if type(center) is Point2D and isinstance(radius, (int, float)) and  radius > 0:
+            self.center = center
+            self.radius = radius
+        else:
+            raise ValueError
+    
+    def __str__(self):
+        return f"Circle: center = {self.center}, radius = {self.radius}"
+
+    def __contains__(self, point: Point2D):
+        if type(point) is Point2D and math.sqrt((point.x - self.center.x) ** 2 + (point.y - self.center.y) ** 2) <= self.radius:
+            return True
+        else: 
+            return False
+    
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
 class Shape2DCollection(Shape2D): #Shape2DCollection* - inherits from Shape2D. 
     """A collection of shapes, stores a list of Shape2D. 
     For Shape2DCollection, area and __contains__ have special meaning:
     area  - is the sum of areas of the stored shapes
     __contains__ - returns True if the point is inside any of the stored shapes."""
-    pass
+    def __init__(self, shapes: list[Shape2D] = []):# -> None:
+        super().__init__()
+        self.shapes = shapes
+    def __str__(self):
+        res = ""
+        for shape in self.shapes:
+            res += str(shape)
+        return res
+    """def add(self, shape: Shape2D):
+        self.shapes.append(shape)"""
+    def __contains__(self, point: Point2D):
+        if type(point) is Point2D:
+            for shape in self.shapes:
+                if point in shape:
+                    return True
+        return False
+    
+    def area(self):
+        res = 0
+        for shape in self.shapes:
+            res += shape.area()
+        return res
 
-
-var = Point2D(5, 6)
-print(var.x)
---
+++ tasks/task08/test_shapes2d.py
@@ -0,0 +1,44 @@
+#tests for Task 08: Class - inheritance, magic methods
+import unittest 
+from shapes2d import Point2D, Shape2D, Rectangle, Circle, Square, Shape2DCollection
+
+class TestShapes2D(unittest.TestCase):
+    def setUp(self):    #before start    
+        self.point1 = Point2D(-1, -2)
+        self.point2 = Point2D(2, -2)
+        self.point3 = Point2D(2, -3)
+        self.point4 = Point2D(-4, -5)
+        self.rect = Rectangle(self.point1, 6, 4)
+        self.sqr = Square(self.point3, 2)
+        self.crcl = Circle(self.point1, 3)
+        self.shapes = Shape2DCollection([self.rect, self.sqr, self.crcl])
+
+    def test_area_rect(self): # OK
+        self.assertEqual(self.rect.area(), 24)
+    def test_area_sqr(self): # OK
+        self.assertEqual(self.sqr.area(), 4)
+    def test_area_crcl(self): # OK
+        self.assertEqual(self.crcl.area(), 28.274333882308138)
+    def test_area_shapes(self): # OK
+        self.assertEqual(self.shapes.area(), 56.27433388230814)
+
+    def test_cont_rect(self): # OK
+        self.assertTrue(self.point2 in self.rect)
+    def test_cont_sqr(self): # OK
+        self.assertTrue(self.point2 in self.sqr)
+    def test_cont_crcl(self): # OK
+        self.assertTrue(self.point2 in self.crcl)
+    def test_cont_shapes(self): # OK
+        self.assertTrue(self.point2 in self.shapes)
+
+    def test_ncont_rect(self): # OK
+        self.assertFalse(self.point4 in self.rect)
+    def test_ncont_sqr(self): # OK
+        self.assertFalse(self.point4 in self.sqr)
+    def test_ncont_crcl(self): # OK
+        self.assertFalse(self.point4 in self.crcl)
+    def test_ncont_shapes(self): # OK
+        self.assertFalse(self.point4 in self.shapes)    
+
+if __name__ == '__main__':  
+    unittest.main()
