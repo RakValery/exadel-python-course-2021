4db1231ad6c3ef228818ea36f147d465607ab2ef
Valery Rak <vrak@exadel.com>
2021-10-13 17:13:22 +0300

    Task 08: Class - inheritance, magic methods v1.1 with tests v1.2

--- tasks/task08/shapes2d.py
+++ tasks/task08/shapes2d.py
@@ -5,50 +5,60 @@ import abc
 import math 
  
 class Point2D(NamedTuple):
-    """
-    A point in 2-dimensional space.
-    Implemented as NamedTuple (so it is immutable), but simple class can be used instead.
-    """
     x: float
     y: float
  
     def __str__(self):
-        return f"({self.x}, {self.y})"
- 
+        return f"({self.x}, {self.y})" 
  
 class Shape2D(abc.ABC):
-    """
-    An abstract shape in 2-dimensional space. Examples of 2D shapes are rectangle, circle, etc.
-    """
- 
     @property
     @abc.abstractmethod
     def area(self):# -> float:
-        """Area of the shape."""
         raise NotImplementedError
  
     @abc.abstractmethod
     def __contains__(self, point: Point2D):# -> bool:
-        """Check if the point is inside the shape.
-        Support semantics like `if point in shape`"""
         raise NotImplementedError
  
     @abc.abstractmethod
     def __str__(self):# -> str:
-        """Get string representation of the shape."""
         raise NotImplementedError
 
 class Rectangle(Shape2D): #Rectangle - inherits from Shape2D. Has arguments like bottom_left, width, length.
     def __init__(self, bottom_left: Point2D, width: float, length: float):
         super().__init__()
-        if type(bottom_left) is Point2D and isinstance(width, (int, float)) and isinstance(length, (int, float)) and  \
-            width > 0 and length > 0:###
-            self.bottom_left = bottom_left
-            self.width = width
-            self.length = length
-        else:
-            raise ValueError
+        self.bottom_left = bottom_left
+        self.width = width
+        self.length = length 
+            
+    @property
+    def bottom_left(self):
+        return self._bottom_left
+    @bottom_left.setter
+    def bottom_left(self, bottom_left):
+        if type(bottom_left) != Point2D:
+            raise ValueError("Type of bottom left point must be Point2D")
+        self._bottom_left = bottom_left
+
+    @property
+    def width(self):
+        return self._width
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, (int, float)) or width <= 0:
+            raise ValueError("Width must be Int or Float positive number")
+        self._width = width
     
+    @property
+    def length(self):
+        return self._length
+    @length.setter
+    def length(self, length):
+        if not isinstance(length, (int, float)) or length <= 0:
+            raise ValueError("Length must be Int or Float positive number")
+        self._length = length
+
     def __str__(self):
         return f"Rectangle: bottom_left = {self.bottom_left}, width = {self.width}, length = {self.length}"
 
@@ -58,29 +68,51 @@ class Rectangle(Shape2D): #Rectangle - inherits from Shape2D. Has arguments like
         else: 
             return False
     
+    @property
     def area(self):
         return self.width * self.length
 
 class Square(Rectangle): #Square - inherits from Rectangle.
     def __init__(self, bottom_left: Point2D, side: float):
-        if type(bottom_left) is Point2D and isinstance(side, (int, float)) and  side > 0:
-            super().__init__(bottom_left, side, side)
-            self.side = side
-        else:
-            raise ValueError
-    
+        self.side = side
+        super().__init__(bottom_left, side, side)
+
+    @property
+    def side(self):
+        return self._side
+    @side.setter
+    def side(self, side):
+        if not isinstance(side, (int, float)) or side <= 0:
+            raise ValueError("Side must be Int or Float positive number")
+        self._side = side   
+
     def __str__(self):
         return f"Square: bottom_left = {self.bottom_left}, side = {self.side}"
     
 class Circle(Shape2D): #Circle - inherits from Shape2D. Has arguments like center, radius.
     def __init__(self, center: Point2D, radius: float):# -> None:
         super().__init__()
-        if type(center) is Point2D and isinstance(radius, (int, float)) and  radius > 0:
-            self.center = center
-            self.radius = radius
-        else:
-            raise ValueError
+        self.center = center
+        self.radius = radius
     
+    @property
+    def radius(self):
+        return self._radius
+    @radius.setter
+    def radius(self, radius):
+        if not isinstance(radius, (int, float)) or radius <= 0:
+            raise ValueError("Radius must be Int or Float positive number")
+        self._radius = radius
+    
+    @property
+    def center(self):
+        return self._center
+    @center.setter
+    def center(self, center):
+        if type(center) != Point2D:
+            raise ValueError("Type of center point must be Point2D")
+        self._center = center
+
     def __str__(self):
         return f"Circle: center = {self.center}, radius = {self.radius}"
 
@@ -90,24 +122,34 @@ class Circle(Shape2D): #Circle - inherits from Shape2D. Has arguments like cente
         else: 
             return False
     
+    @property
     def area(self):
         return math.pi * (self.radius ** 2)
 
 class Shape2DCollection(Shape2D): #Shape2DCollection* - inherits from Shape2D. 
-    """A collection of shapes, stores a list of Shape2D. 
-    For Shape2DCollection, area and __contains__ have special meaning:
-    area  - is the sum of areas of the stored shapes
-    __contains__ - returns True if the point is inside any of the stored shapes."""
     def __init__(self, shapes: list[Shape2D] = []):# -> None:
         super().__init__()
         self.shapes = shapes
+
+    @property
+    def shapes(self):
+        return self._shapes
+    @shapes.setter
+    def shapes(self, shapes):
+        for shape in shapes:
+            if not isinstance(shape, Shape2D):
+                raise ValueError(f"Type of collection item must be Shape2D")
+        self._shapes = shapes
+
     def __str__(self):
-        res = ""
+        res = f"Collection has {len(self.shapes)} shapes:"
         for shape in self.shapes:
-            res += str(shape)
+            res += "\n" + str(shape)
         return res
-    """def add(self, shape: Shape2D):
-        self.shapes.append(shape)"""
+
+    #def add(self, shape: Shape2D):
+    #    self.shapes.append(shape)
+
     def __contains__(self, point: Point2D):
         if type(point) is Point2D:
             for shape in self.shapes:
@@ -115,9 +157,9 @@ class Shape2DCollection(Shape2D): #Shape2DCollection* - inherits from Shape2D.
                     return True
         return False
     
+    @property
     def area(self):
         res = 0
         for shape in self.shapes:
-            res += shape.area()
+            res += shape.area
         return res
-
--- tasks/task08/test_shapes2d.py
+++ tasks/task08/test_shapes2d.py
@@ -14,13 +14,13 @@ class TestShapes2D(unittest.TestCase):
         self.shapes = Shape2DCollection([self.rect, self.sqr, self.crcl])
 
     def test_area_rect(self): # OK
-        self.assertEqual(self.rect.area(), 24)
+        self.assertEqual(self.rect.area, 24)
     def test_area_sqr(self): # OK
-        self.assertEqual(self.sqr.area(), 4)
+        self.assertEqual(self.sqr.area, 4)
     def test_area_crcl(self): # OK
-        self.assertEqual(self.crcl.area(), 28.274333882308138)
+        self.assertEqual(self.crcl.area, 28.274333882308138)
     def test_area_shapes(self): # OK
-        self.assertEqual(self.shapes.area(), 56.27433388230814)
+        self.assertEqual(self.shapes.area, 56.27433388230814)
 
     def test_cont_rect(self): # OK
         self.assertTrue(self.point2 in self.rect)
@@ -31,14 +31,35 @@ class TestShapes2D(unittest.TestCase):
     def test_cont_shapes(self): # OK
         self.assertTrue(self.point2 in self.shapes)
 
-    def test_ncont_rect(self): # OK
+    def test_ncont_rect(self): # False
         self.assertFalse(self.point4 in self.rect)
-    def test_ncont_sqr(self): # OK
+    def test_ncont_sqr(self): # False
         self.assertFalse(self.point4 in self.sqr)
-    def test_ncont_crcl(self): # OK
+    def test_ncont_crcl(self): # False
         self.assertFalse(self.point4 in self.crcl)
-    def test_ncont_shapes(self): # OK
+    def test_ncont_shapes(self): # False
         self.assertFalse(self.point4 in self.shapes)    
+
+    def test_init_rect(self): # OK
+        with self.assertRaisesRegex(ValueError, "Type of bottom left point must be Point2D"):
+            Rectangle({"a": 2, "b": 6}, 5, 9)
+        with self.assertRaisesRegex(ValueError, "Width must be Int or Float positive number"):
+            Rectangle(self.point4, "5", 9)
+        with self.assertRaisesRegex(ValueError, "Length must be Int or Float positive number"):
+            Rectangle(self.point4, 5, "9")
+    def test_init_sqr(self): # OK
+        with self.assertRaisesRegex(ValueError, "Type of bottom left point must be Point2D"):
+            Square({"a": 2, "b": 6}, 5)
+        with self.assertRaisesRegex(ValueError, "Side must be Int or Float positive number"):
+            Square(self.point4, "5")
+    def test_init_crcl(self): # OK
+        with self.assertRaisesRegex(ValueError, "Type of center point must be Point2D"):
+            Circle({"a": 2, "b": 6}, 5)
+        with self.assertRaisesRegex(ValueError, "Radius must be Int or Float positive number"):
+            Circle(self.point4, "5")        
+    def test_init_shapes(self): # OK
+        with self.assertRaisesRegex(ValueError, "Type of collection item must be Shape2D"):
+            Shape2DCollection([self.rect, self.point3, self.crcl])
 
 if __name__ == '__main__':  
     unittest.main()
